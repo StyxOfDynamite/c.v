8dcb8d50059cdbb041bafd7ddaa95902305c36b5
lukes <lads89@gmail.com>
2021-03-04 14:27:07 +0000

    updates.

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,58 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: "Luke Storer",
+  title: "Software Engineer",
   facts: {
-    'Residence': 'London',
-    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-                  <span class="screen">wiki</span>
+    Residence: "Leicester",
+    Homepage: `<a href="https://lukestorer.dev">
+                  <span class="print">https://lukestorer.dev</span>
+                  <span class="screen">https://lukestorer.dev</span>
                 </a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
+    Email: '<a href="mailto:hello@lukestorer.dev">hello@lukestorer.dev</a>',
   },
-  skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
-  ],
-  plays: [
+  techstack: ["PHP", "SQL", "NoSQL", "Cloud", "JS"],
+  jobs: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: "Senior Learning Platform Engineer ",
+      company: "Avado",
+      dates: "SEP 2020 - PRESENT",
+      badges: ["laravel", "symfony", "react", "SQL-Server", "PostgreSQL"],
+      contents: `I lead a team of 5, helping to build and maintain a learning platform solution, using a mix of PHP and
+      Javascript. Our approach is focused on a RESTful API Backend, and a SPA Frontend written using
+      React. In my role as a team lead, I am responsible for the well being of my direct reports, as well as
+      the weekly delivery of work from the team to production servers. As a senior I am also responsible in
+      part for deployment of the completed work to the production infrastructure`,
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
+      title: "Twelfth Night",
+      written: "1600 – 1601",
+      badges: ["comedy", "love"],
       contents: `
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+`,
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
+      title: "The Tragedy of Romeo and Juliet",
+      written: "1595–1596, with a possible early draft written in 1591",
+      badges: ["tragedy", "death", "love"],
       contents: `
 In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
+`,
+    },
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
+      header: "Performance",
+      skills: ["theater", "play"],
       contents: `
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+`,
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
+      header: "Poetry",
+      skills: ["poetry", "erotic"],
       contents: `
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ]
+`,
+    },
+  ],
 };
