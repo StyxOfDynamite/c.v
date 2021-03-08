dfee5b43111378da4d27eeaedaf20b7114707e59
lukes <lads89@gmail.com>
2021-03-08 20:44:28 +0000

    update content.

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,13 +9,24 @@ module.exports = {
                 </a>`,
     Email: '<a href="mailto:hello@lukestorer.dev">hello@lukestorer.dev</a>',
   },
-  techstack: ["PHP", "SQL", "NoSQL", "Cloud", "JS"],
+  techstack: [
+    "PHP",
+    "Laravel",
+    "Symfony",
+    "Slim",
+    "MySQL",
+    "NoSQL",
+    "AWS",
+    "JS",
+    "React",
+    "Vue",
+  ],
   jobs: [
     {
       title: "Senior Learning Platform Engineer ",
       company: "Avado",
-      dates: "SEP 2020 - PRESENT",
-      badges: ["laravel", "symfony", "react", "SQL-Server", "PostgreSQL"],
+      dates: "2020 - PRESENT",
+      badges: ["Laravel", "Symfony", "React", "SQL-Server", "PostgreSQL"],
       contents: `I lead a team of 5, helping to build and maintain a learning platform solution, using a mix of PHP and
       Javascript. Our approach is focused on a RESTful API Backend, and a SPA Frontend written using
       React. In my role as a team lead, I am responsible for the well being of my direct reports, as well as
@@ -23,36 +34,55 @@ module.exports = {
       part for deployment of the completed work to the production infrastructure`,
     },
     {
-      title: "Twelfth Night",
-      written: "1600 – 1601",
-      badges: ["comedy", "love"],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`,
+      title: "Senior Web Developer",
+      company: "REVIEWS.io",
+      dates: "2017 - 2020",
+      badges: ["Laravel", "AWS", "AngularJS", "MySQL", "Elasticsearch"],
+      contents: `Working with PHP, Javascript and NoSQL and SQL Databases.
+      I assisted in the development and maintenance of a world leading, review collection platform.
+      This involved building functionality, fixing issues and liaising with customers to solve integration issues.`,
     },
     {
-      title: "The Tragedy of Romeo and Juliet",
-      written: "1595–1596, with a possible early draft written in 1591",
-      badges: ["tragedy", "death", "love"],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`,
+      title: "PHP Web Developer",
+      company: "eSterling",
+      dates: "2015 - 2016",
+      badges: [
+        "Laravel",
+        "AWS",
+        "AngularJS",
+        "MySQL",
+        "Wordpress",
+        "Opencart",
+        "Magento",
+        "Shopify",
+      ],
+      contents: `Working at a web development and design agency, I was tasked with PHP development across a
+      variety of Open Source products, including Wordpress, Magento and Opencart. I assisted in delivery
+      of bespoke functionality for a number of client sites and products.`,
+    },
+    {
+      title: "Software Support Engineer",
+      company: "Jadu",
+      dates: "2012 - 2015",
+      badges: ["PHP", "Rackspace", "HTML", "MySQL", "CSS"],
+      contents: `As a member of the support team, I was tasked with issue resolution for various bugs in the PHP
+      platform code. Following stringent development and release practices. Building and supporting an
+      in-house ticketing system and helping local government clients resolve issues with their site.`,
     },
   ],
   experience: [
     {
-      header: "Performance",
-      skills: ["theater", "play"],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`,
+      header: "Asynchronous Review Booster ",
+      skills: ["SQS", "CSV"],
+      contents: `I assisted in the design, development and deployment of the revised asynchronous Review Booster module,
+      this utilised off loading work to a an SQS Message Queue and completed processing of large CSV documents in the background,
+      notifying users upon completion`,
     },
     {
-      header: "Poetry",
-      skills: ["poetry", "erotic"],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`,
+      header: "Drag and Drop File Upload",
+      skills: ["React", "HTML5"],
+      contents: `Using React I built a component for uploading profile images to a RESTful API,
+      it utilised HTML5 Drag and Drop functionality, as well as a standard files input to offer better UX`,
     },
   ],
 };
